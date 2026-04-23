5ca8bfc7c2709c7a50d970c6467df6227720d090
Ivan Hudko <giv.10072007@gmail.com>
2026-04-23 22:35:38 +0300

    fix Input name param

--- frontend/project_z_frontend/app/features/titleFilter/ui/StatusFilter.tsx
+++ frontend/project_z_frontend/app/features/titleFilter/ui/StatusFilter.tsx
@@ -3,18 +3,20 @@ import { Button } from "~/shared/ui/Button";
 import { useTitleFilterStore } from "../store/titleFilter.store";
 
 const statusOptions = [
-    { label: "All", value: undefined },
-    { label: "In Progress", value: Status.INPROGRESS },
-    { label: "Planned", value: Status.PLANNED },
-    { label: "Watched", value: Status.WATCHED },
-    { label: "Dropped", value: Status.DROPPED },
-  ];
+  { label: "All", value: undefined },
+  { label: "In Progress", value: Status.INPROGRESS },
+  { label: "Planned", value: Status.PLANNED },
+  { label: "Watched", value: Status.WATCHED },
+  { label: "Dropped", value: Status.DROPPED },
+];
 
-  const StatusFilter = () => {
+const StatusFilter = () => {
   const { status, setStatus } = useTitleFilterStore();
   return (
     <div className="flex flex-col gap-2">
-      <label className="text-sm font-bold text-foreground-muted uppercase px-1">Status</label>
+      <label className="text-sm font-bold text-foreground-muted uppercase px-1">
+        Status
+      </label>
       <div className="flex flex-wrap gap-2">
         {statusOptions.map((opt) => (
           <Button
@@ -22,9 +24,10 @@ const statusOptions = [
             key={opt.label}
             onClick={() => setStatus(opt.value)}
             className={`border-none px-4 py-2 rounded-full text-sm font-semibold transition-all ${
-              (status === opt.value || (opt.value === undefined && status === undefined))
-                ? 'bg-primary text-foreground shadow-md shadow-yellow-100'
-                : 'bg-background-muted text-foreground hover:bg-background-muted-hover hover:text-foreground-muted '
+              status === opt.value ||
+              (opt.value === undefined && status === undefined)
+                ? "bg-primary text-foreground shadow-xs shadow-yellow-100"
+                : "bg-background-muted text-foreground hover:bg-background-muted-hover hover:text-foreground-muted "
             }`}
           >
             {opt.label}
--- frontend/project_z_frontend/app/widgets/WatchListRow/watchlistRow.tsx
+++ frontend/project_z_frontend/app/widgets/WatchListRow/watchlistRow.tsx
@@ -1,12 +1,15 @@
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { useState } from "react";
 import { useNavigate } from "react-router";
-import { CompactRate, type Status, type TitleRecord } from "~/entities/titleRecord";
+import {
+  CompactRate,
+  type Status,
+  type TitleRecord,
+} from "~/entities/titleRecord";
 import { useUpdateTitleRecord } from "~/entities/titleRecord/hooks/useTitleRecordUpdateMutation";
 import { StatusSelect } from "~/features/manageTitle";
 import { Button } from "~/shared/ui/Button";
 import { Input } from "~/shared/ui/Input";
-
 
 interface WatchlistRowProps {
   title: TitleRecord;
@@ -14,12 +17,12 @@ interface WatchlistRowProps {
 }
 export const WatchlistRow = ({ title, isOwn }: WatchlistRowProps) => {
   const [tempTitleName, setTempTitleName] = useState(title.titleName);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { updateTitle, deleteTitle } = useUpdateTitleRecord(title.titleId);
   const handleImageClick = (e: React.MouseEvent) => {
     e.stopPropagation();
     if (title.apiTitleId) {
-      navigate(`/anime/${title.apiTitleId}`)
+      navigate(`/anime/${title.apiTitleId}`);
     }
   };
   const handleDelete = (e: React.MouseEvent) => {
@@ -27,34 +30,47 @@ export const WatchlistRow = ({ title, isOwn }: WatchlistRowProps) => {
     deleteTitle();
   };
 
-return (
+  return (
     <div className="group flex flex-col sm:flex-row items-stretch sm:items-center gap-2 sm:gap-4 bg-card p-2 rounded-xl border border-border transition-all w-full">
-      
       <div className="flex items-center flex-1 gap-3 min-w-0">
         {isOwn && (
           <div onPointerDown={(e) => e.stopPropagation()}>
-            <Button variant="fill" onClick={handleDelete} className="p-1.5 bg-danger hover:bg-danger-hover rounded-lg">
+            <Button
+              variant="fill"
+              onClick={handleDelete}
+              className="p-1.5 bg-danger hover:bg-danger-hover rounded-lg"
+            >
               <DeleteOutlineIcon sx={{ fontSize: 16 }} />
             </Button>
           </div>
         )}
 
-        <div className="relative h-10 w-16 flex-shrink-0 transition-transform duration-500 hover:scale-[2.5] hover:z-10"  onPointerDown={(e) => e.stopPropagation()}>
-          <img 
+        <div
+          className="relative h-10 w-16 flex-shrink-0 transition-transform duration-500 hover:scale-[2.5] hover:z-10"
+          onPointerDown={(e) => e.stopPropagation()}
+        >
+          <img
             src={title.imageUrl || "/defautlTitleRecordImage.jpg"}
             onClick={handleImageClick}
-            className="absolute inset-0 h-full w-full object-cover rounded-md cursor-pointer" 
+            className="absolute inset-0 h-full w-full object-cover rounded-md cursor-pointer"
             alt={title.titleName}
           />
         </div>
 
-        <div className="flex-1 min-w-0" onPointerDown={(e) => e.stopPropagation()}>
+        <div
+          className="flex-1 min-w-0"
+          onPointerDown={(e) => e.stopPropagation()}
+        >
           {isOwn ? (
-            <Input 
+            <Input
+              name="Title name"
               type="text"
               value={tempTitleName}
               onChange={(val) => setTempTitleName(val)}
-              onBlur={() => tempTitleName !== title.titleName && updateTitle({ titleName: tempTitleName })}
+              onBlur={() =>
+                tempTitleName !== title.titleName &&
+                updateTitle({ titleName: tempTitleName })
+              }
               className="w-full font-bold text-foreground uppercase text-xs sm:text-sm bg-transparent border-none p-0 h-auto leading-tight focus:ring-0"
             />
           ) : (
@@ -66,14 +82,17 @@ return (
       </div>
 
       <div className="flex items-center justify-between sm:justify-end gap-3 sm:w-auto mt-2 sm:mt-0">
-        <div className="flex-shrink-0" onPointerDown={(e) => e.stopPropagation()}>
+        <div
+          className="flex-shrink-0"
+          onPointerDown={(e) => e.stopPropagation()}
+        >
           {isOwn ? (
             <div className="w-32 sm:w-40">
-              <StatusSelect 
-                variant="page" 
+              <StatusSelect
+                variant="page"
                 initialData={title}
                 titleRecord={title}
-                className="h-9 text-xs font-bold bg-transparent rounded-lg" 
+                className="h-9 text-xs font-bold bg-transparent rounded-lg"
               />
             </div>
           ) : (
@@ -83,11 +102,20 @@ return (
           )}
         </div>
 
-        <div onPointerDown={(e) => e.stopPropagation()} className={!isOwn ? "pointer-events-none opacity-90" : ""}>
-          <CompactRate 
+        <div
+          onPointerDown={(e) => e.stopPropagation()}
+          className={!isOwn ? "pointer-events-none opacity-90" : ""}
+        >
+          <CompactRate
             currentRating={title.rating?.overall}
-            onRate={isOwn ? (val) => updateTitle({ rating: { overall: val } }) : undefined}
-            onClear={isOwn ? () => updateTitle({ rating: undefined }) : undefined}
+            onRate={
+              isOwn
+                ? (val) => updateTitle({ rating: { overall: val } })
+                : undefined
+            }
+            onClear={
+              isOwn ? () => updateTitle({ rating: undefined }) : undefined
+            }
           />
         </div>
       </div>
