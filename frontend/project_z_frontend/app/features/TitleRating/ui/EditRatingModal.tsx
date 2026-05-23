bd18193dd36291d45a9a827844877a83beaa21d8
Ivan Hudko <giv.10072007@gmail.com>
2026-05-23 16:26:04 +0300

    fix: after review

--- frontend/project_z_frontend/app/features/TitleRating/ui/EditRatingModal.tsx
+++ frontend/project_z_frontend/app/features/TitleRating/ui/EditRatingModal.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import Modal from "~/shared/ui/Modal/Modal";
-import { type TitleRecord, useTitleRecordMutation } from "~/entities/titleRecord";
+import {
+  type TitleRecord,
+  TitleType,
+  useTitleRecordMutation,
+} from "~/entities/titleRecord";
 import { RatingEditorContent } from "./RatingEditorContent";
 import type { Rating } from "~/shared/types";
 
@@ -10,7 +14,11 @@ interface EditRatingModalProps {
   onClose: () => void;
 }
 
-export const EditRatingModal = ({ title, isOpen, onClose }: EditRatingModalProps) => {
+export const EditRatingModal = ({
+  title,
+  isOpen,
+  onClose,
+}: EditRatingModalProps) => {
   const { rate } = useTitleRecordMutation(
     title.apiTitleId,
     {
@@ -19,12 +27,13 @@ export const EditRatingModal = ({ title, isOpen, onClose }: EditRatingModalProps
       status: title.status,
       rating: title.rating,
       imageUrl: title.imageUrl,
+      titleType: title.titleType,
     },
-    title
+    title,
   );
 
   const [localRatings, setLocalRatings] = useState<Rating>(
-    title.rating && "overall" in title.rating ? title.rating : { overall: 0 }
+    title.rating && "overall" in title.rating ? title.rating : { overall: 0 },
   );
 
   useEffect(() => {
@@ -34,8 +43,9 @@ export const EditRatingModal = ({ title, isOpen, onClose }: EditRatingModalProps
   }, [isOpen, title.rating]);
 
   const handleSave = () => {
-    const hasChanges = JSON.stringify(localRatings) !== JSON.stringify(title.rating);
-    
+    const hasChanges =
+      JSON.stringify(localRatings) !== JSON.stringify(title.rating);
+
     if (hasChanges) {
       rate(localRatings);
     }
@@ -45,17 +55,17 @@ export const EditRatingModal = ({ title, isOpen, onClose }: EditRatingModalProps
   return (
     <Modal
       isOpen={isOpen}
-      onClose={handleSave} 
+      onClose={handleSave}
       title={`Rating "${title.titleName}"`}
       maxWidth="max-w-xl"
     >
       <RatingEditorContent
         titleId={title.titleId}
         ratings={localRatings}
-        onChange={setLocalRatings} 
-        isSaving={false} 
+        onChange={setLocalRatings}
+        isSaving={false}
         onSave={handleSave}
-        onCancel={onClose}  
+        onCancel={onClose}
       />
     </Modal>
   );
