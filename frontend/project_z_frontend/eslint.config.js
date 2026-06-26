3be357b84d221b08db116f0f3c20907b8b005ff0
Ivan Hudko <giv.10072007@gmail.com>
2026-06-26 11:34:10 +0300

    chore: add linting and formatting tools

---
+++ frontend/project_z_frontend/eslint.config.js
@@ -0,0 +1,51 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import fsdPlugin from "@conarti/eslint-plugin-feature-sliced";
+
+export default [
+  { ignores: ["dist", "build", ".react-router"] },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "@conarti/feature-sliced": fsdPlugin,
+    },
+    rules: {
+      "@conarti/feature-sliced/public-api": "error",
+
+      "@conarti/feature-sliced/layers-slices": ["error", { ignoreLayer: ["app"] }],
+
+      "@conarti/feature-sliced/absolute-relative": "error",
+    },
+  },
+
+  eslintConfigPrettier,
+];
---
+++ frontend/project_z_frontend/types.d.ts
@@ -0,0 +1,1 @@
+declare module '@conarti/eslint-plugin-feature-sliced';
