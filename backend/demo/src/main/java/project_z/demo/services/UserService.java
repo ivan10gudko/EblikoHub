2b0340b43b475257c2f0c1d7e4f0a595c819cbc5
Walpys <artemkrat073@gmail.com>
2026-07-02 17:03:38 +0300

    add user search with included relations to room

--- backend/demo/src/main/java/project_z/demo/services/UserService.java
+++ backend/demo/src/main/java/project_z/demo/services/UserService.java
@@ -8,15 +8,26 @@ import org.springframework.web.multipart.MultipartFile;
 
 import project_z.demo.common.QueryParameters.UserQueryParameters;
 import project_z.demo.dto.UserDtos.UserDto;
+import project_z.demo.dto.UserDtos.UserWithRelationsToRoomDto;
 import project_z.demo.entity.UserEntity;
 
 public interface UserService {
     public UserEntity save(UserEntity user);
+
     UserEntity findOne(UUID id);
+
     boolean isExists(UUID id);
+
     UserEntity partialUpdate(UUID id, UserEntity userEntity);
+
     void deleteById(UUID id);
+
     UserDto findByNameTag(String nameTag);
+
     String uploadAvatar(UserEntity userEntity, MultipartFile file);
+
     Page<UserEntity> findByName(String name, UserQueryParameters userQueryParameters, UUID currentUserId);
+
+    Page<UserWithRelationsToRoomDto> searchUsersForRoom(String name, Long roomId,
+            UserQueryParameters queryParameters);
 }
